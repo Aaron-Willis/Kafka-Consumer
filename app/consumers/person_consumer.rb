6b2f812cc1de50772348d4d9a6d1e77b6525cb34
nkirby-va <nicholas.kirby@va.gov>
2023-12-26 15:59:47 -0500

    add simple local deserialization example that is in the docs

---
+++ app/consumers/person_consumer.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Example consumer that prints messages payloads
+class PersonConsumer < ApplicationConsumer
+  def consume
+    messages.each { |message| puts message.payload }
+  end
+
+  # Run anything upon partition being revoked
+  # def revoked
+  # end
+
+  # Define here any teardown things you want when Karafka server stops
+  # def shutdown
+  # end
+end
+
+# Notes:
+# avro = AvroTurf.new(schemas_path: 'lib/avro/schemas/')
+# message = avro.encode({ 'full_name' => 'Nick', 'age' => 30 }, schema_name: 'person', validate: true)
+# avro.decode(message, schema_name: "person")
